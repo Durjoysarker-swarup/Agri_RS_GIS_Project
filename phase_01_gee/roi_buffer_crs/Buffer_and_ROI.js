968ec10e3ab18a6440e83faad3b8d4531275a248
Durjoy Kumar Sarker <Durjoysarker78@gmail.com>
2026-06-01 04:16:56 +0600

    Create Buffer_and_ROI.js

---
+++ phase_01_gee/roi_buffer_crs/Buffer_and_ROI.js
@@ -0,0 +1,16 @@
+// Define ROI
+var roi = ee.Geometry.Point([91.8, 24.9]).buffer(1000);
+Map.centerObject(roi, 12);
+Map.addLayer(roi, {color: 'red'}, 'ROI');
+
+// Apply Buffers
+var expand = roi.buffer(500);
+var shrink = roi.buffer(-300);
+
+Map.addLayer(expand, {color: 'blue'}, 'Expanded');
+Map.addLayer(shrink, {color: 'green'}, 'Shrinked');
+
+// Compare Area
+print('Original area', roi.area());
+print('Expanded area', expand.area());
+print('Shrinked area', shrink.area());
