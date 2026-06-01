c44ff734f89d5b6580e35185399feb3a5bc2c86b
Durjoy Kumar Sarker <Durjoysarker78@gmail.com>
2026-06-01 16:25:13 +0600

    Add NDVI extraction and export script for Sentinel-2
    This script extracts NDVI from Sentinel-2 imagery, applies cloud masking, and exports the masked NDVI as a GeoTIFF.

---
+++ phase_01_5_validation_layer/QGIS/4_NDVI_Classification_Mapping/Extracting_NVDI_TIFF.js
@@ -0,0 +1,56 @@
+// REGION OF INTEREST
+var roi = ee.Geometry.Rectangle([91.7, 24.7, 92.0, 25.0]);
+
+Map.centerObject(roi, 10);
+Map.addLayer(roi, {color: 'red'}, 'ROI');
+
+
+// LOAD SENTINEL-2 DATA
+var collection = ee.ImageCollection('COPERNICUS/S2_SR')
+  .filterBounds(roi)
+  .filterDate('2023-01-01', '2023-12-31')
+  .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE', 80))
+  .select(['B4', 'B8', 'SCL']);
+
+// 3. CLOUD MASK FUNCTION
+function maskS2(image) {
+  var scl = image.select('SCL');
+
+  var mask = scl.neq(3)   // cloud shadow
+    .and(scl.neq(8))      // medium cloud
+    .and(scl.neq(9))      // high cloud
+    .and(scl.neq(10))     // cirrus
+    .and(scl.neq(11));    // snow
+
+  return image.updateMask(mask);
+}
+
+
+
+// MASKED NDVI PIPELINE
+var cleanCollection = collection.map(maskS2);
+var cleanComposite = cleanCollection.median().clip(roi);
+
+var ndvi_masked = cleanComposite
+  .normalizedDifference(['B8', 'B4'])
+  .rename('NDVI_MASKED');
+
+
+
+//EXPORT SETTINGS
+var exportRegion = roi;
+var scale = 10;
+
+
+
+// EXPORT 2 — MASKED NDVI
+Export.image.toDrive({
+  image: ndvi_masked,
+  description: 'NDVI_Masked_2023',
+  folder: 'GEE_exports',
+  fileNamePrefix: 'NDVI_masked_2023',
+  region: exportRegion,
+  scale: scale,
+  maxPixels: 1e13,
+  fileFormat: 'GeoTIFF'
+});
